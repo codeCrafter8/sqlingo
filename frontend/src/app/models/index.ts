51dd37983154bde676aeb4041f071803e12a99ff
zuzannawozniak <zuzanna.wk@interia.pl>
2026-03-15 18:46:45 +0100

    feat: add models

---
+++ frontend/src/app/models/index.ts
@@ -0,0 +1,3 @@
+export * from './query-request.model';
+export * from './query-response.model';
+export * from './query-status.enum';
---
+++ frontend/src/app/models/query-status.enum.ts
@@ -0,0 +1,6 @@
+export enum QueryStatus {
+  SUCCESS = 'SUCCESS',
+  INVALID_SQL = 'INVALID_SQL',
+  ERROR = 'ERROR',
+  PENDING = 'PENDING'
+}
