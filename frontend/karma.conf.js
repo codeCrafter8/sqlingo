00c04432752477deb8b9a07638ffcc8edb53552e
zuzannawozniak <zuzanna.wk@interia.pl>
2026-03-19 09:16:03 +0100

    fix(ci): fix run frontend tests

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,23 +1,5 @@
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/build/karma'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular/build/karma'),
-    ],
-    client: {
-      jasmine: {},
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage'),
-      subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }],
-    },
-    reporters: ['progress', 'kjhtml'],
     browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
@@ -25,6 +7,5 @@ module.exports = function (config) {
         flags: ['--no-sandbox', '--disable-setuid-sandbox'],
       },
     },
-    restartOnFileChange: true,
   });
 };
